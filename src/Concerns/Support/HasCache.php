42b7ec6dac70c834fa6db206e5b360c87ea67808
hamzah <hamzahnuralfalah@gmail.com>
2025-03-23 20:38:04 +0700

    update cache

--- src/Concerns/Support/HasCache.php
+++ src/Concerns/Support/HasCache.php
@@ -42,30 +42,37 @@ trait HasCache
 
     public function getCache($key, mixed $tags = null, $default = null)
     {
-        $cache = cache();
-        if (isset($tags)) $cache = $cache->tags($tags);
-        return $cache->get($key, $default);
+        return $this->cacheDriver(function($cache_driver) use ($key,$tags,$default){
+            $cache = cache();
+            if (isset($tags) && $cache_driver == 'redis') $cache = $cache->tags($tags);
+            return $cache->get($key, $default);
+        });
     }
 
 
     public function forgetKey($key, mixed $tags = null)
     {
-        $cache = cache();
-        if (isset($tags)) $cache = $cache->tags($tags);
-        return $cache->forget($key);
+        return $this->cacheDriver(function($cache_driver) use ($key,$tags){
+            $cache = cache();
+            if (isset($tags) && $cache_driver == 'redis') $cache = $cache->tags($tags);
+            return $cache->forget($key);
+        });
     }
 
     public function forgetTags(array|string $tags = [])
     {
-        $tags = $this->mustArray($tags);
-        if ($cacheDriver === 'redis') {
-            return Cache::tags($tags)->flush();
-        } elseif ($cacheDriver === 'database') {
-            // Perform actions specific to Database cache
-        } else {
-            // Handle other cache drivers or default actions
-        }
-        return null;
+        return $this->cacheDriver(function($cache_driver) use ($tags){
+            if ($cache_driver === 'redis') {
+                $tags = $this->mustArray($tags);
+                return Cache::tags($tags)->flush();
+            }
+            return null;
+        });
+    }
+
+    public function cacheDriver(callable $callback){
+        $cache_driver = config('cache.default','database');
+        return $callback($cache_driver);
     }
 
     /**
