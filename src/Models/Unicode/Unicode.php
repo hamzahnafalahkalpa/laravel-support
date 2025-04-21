07393d1d727e749f36621f6a0531f171a28b2a67
Hamzah <hamzahnafalah@gmail.com>
2025-04-22 04:14:41 +0700

    update modules

--- src/Models/Unicode/Unicode.php
+++ src/Models/Unicode/Unicode.php
@@ -4,12 +4,20 @@ namespace Hanafalah\LaravelSupport\Models\Unicode;
 
 use Hanafalah\LaravelHasProps\Concerns\HasProps;
 use Hanafalah\LaravelSupport\Models\BaseModel;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Unicode extends BaseModel
 {
-    use HasProps;
+    use HasProps, SoftDeletes;
 
     protected $table    = 'unicodes';
-    public $timestamps  = false;
-    protected $fillable = ['id', 'unicode_type', 'flag', 'name'];
+    protected $fillable = ['id', 'flag', 'name', 'props'];
+
+    public function viewUsingRelation():array {
+        return [];
+    }
+
+    public function showUsingRelation():array {
+        return [];
+    }
 }
