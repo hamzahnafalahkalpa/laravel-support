8696c263efb8edb8b6a5e9cf31621baf512636e8
Hamzah <hamzahnafalah@gmail.com>
2025-07-13 05:15:40 +0700

    update modules

--- src/Models/PayloadMonitoring/PayloadMonitoring.php
+++ src/Models/PayloadMonitoring/PayloadMonitoring.php
@@ -5,12 +5,15 @@ namespace Hanafalah\LaravelSupport\Models\PayloadMonitoring;
 use Hanafalah\LaravelSupport\Models\{
     BaseModel
 };
+use Illuminate\Database\Eloquent\Concerns\HasUlids;
 
 class PayloadMonitoring extends BaseModel
 {
+    use HasUlids;
+
     public $incrementing = false;
     protected $keyType = 'string';
-    protected $primaryKey = 'string';
+    protected $primaryKey = 'id';
 
     protected $fillable = [
         'id',
